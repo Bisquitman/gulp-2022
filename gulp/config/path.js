d772d3e5a6f8ae632ac805cd6c837edb645b1c55
Bisquitman <lynx8309@gmail.com>
2022-02-10 00:49:24 +0300

    first commit

---
+++ gulp/config/path.js
@@ -0,0 +1,38 @@
+// Получаем имя папки проекта
+import * as nodePath from 'path';
+const rootFolder = nodePath.basename(nodePath.resolve());
+
+const buildFolder = `./dist`, // Также можно использовать rootFolder
+  srcFolder = `./src`;
+
+export const path = {
+  build: {
+    js: `${buildFolder}/js/`,
+    images: `${buildFolder}/img/`,
+    css: `${buildFolder}/css/`,
+    html: `${buildFolder}/`,
+    files: `${buildFolder}/assets/`,
+    fonts: `${buildFolder}/assets/fonts/`,
+  },
+  src: {
+    js: `${srcFolder}/js/app.js`,
+    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+    svg: `${srcFolder}/img/**/*.svg`,
+    scss: `${srcFolder}/scss/style.scss`,
+    html: `${srcFolder}/*.html`,
+    files: `${srcFolder}/assets/**/*.*`,
+    svgicons: `${srcFolder}/svgicons/*.svg`,
+  },
+  watch: {
+    js: `${srcFolder}/js/**/*.js`,
+    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp}`,
+    scss: `${srcFolder}/scss/style.scss`,
+    html: `${srcFolder}/**/*.html`,
+    files: `${srcFolder}/assets/**/*.*`,
+  },
+  clean: buildFolder,
+  buildFolder: buildFolder,
+  srcFolder: srcFolder,
+  rootFolder: rootFolder,
+  ftp: `test`,
+};
---
+++ gulp/tasks/svgSprite.js
@@ -0,0 +1,20 @@
+import svgSprite from "gulp-svg-sprite";
+
+export const svgsprite = () => {
+  return app.gulp.src(app.path.src.svgicons)
+    .pipe(app.plugins.plumber(
+      app.plugins.notify.onError({
+        title: "SVGSPRITE",
+        message: "Error: <%= error message %>"
+      })))
+    .pipe(svgSprite({
+      mode: {
+        stack: {
+          sprite: `../icons/icons_sprite.svg`,
+          // Создавать ли страницу с перечнем иконок
+          example: app.isDev,
+        },
+      },
+    }))
+    .pipe(app.gulp.dest(`${app.path.build.images}`))
+};
